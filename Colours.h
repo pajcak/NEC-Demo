8a8ff7921f00050990ab689bcc1860136f4ceb16
Patrik Faistaver <Patrik.Faistaver@centrum.cz>
2015-05-09 01:07:38 +0200

    license_added

--- Colours.h
+++ Colours.h
@@ -1,9 +1,16 @@
-/* 
- * File:   Colours.h
- * Author: Patrik Faistaver
- *
- * Created on May 31, 2014, 12:39 AM
- */
+//    File: Colours.h
+//    Copyright (C) 2015  Patrik Faistaver
+//    
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
 
 #include <map>
 #include <iostream>
--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
-/* 
- * File:   main.cpp
- * Author: Patrik Faistaver
- *
- * Created on May 17, 2014, 7:08 PM
- */
+//    File: main.cpp
+//    Copyright (C) 2015  Patrik Faistaver
+//    
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+
 
 #include <cstdlib>
 #include <ctime>
